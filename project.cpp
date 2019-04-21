a6a30df4b3fd10c4331e6c80d2491b42509f91e4
Ma9si <46700948+Ma9si@users.noreply.github.com>
2019-04-22 00:12:06 +0530

    Addition of 2 matrix

--- project.cpp
+++ project.cpp
@@ -210,7 +210,7 @@ int power2()                                                         //function 
 
 /*.....................................................*/
 
-int sortarray()
+int sortarray()                                                    //function to sort elements in array in ascending order
 {
     int arr[100];
     int size, i, j, temp;
@@ -248,3 +248,49 @@ int sortarray()
  
     return 0;
 }
+
+/*....................................................*/
+
+int addmatrix()                                                  //function for addition of two matrix
+{
+	int x[3][3],y[3][3],z[3][3],i,j;
+cout<<"ENTER ELEMENTS OF 1st MATRIX\n";
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+cin>>x[i][j];
+}
+cout<<"ENTER ELEMENTS OF 2nd MATRIX\n";
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+cin>>y[i][j];
+}
+cout<<"MATRIX [X]";
+for(i=0;i<3;i++)
+{
+cout<<"\n\n";
+for(j=0;j<3;j++)
+cout<<x[i][j]<<"\t";
+}
+cout<<"\nMATRIX [Y]";
+for(i=0;i<3;i++)
+{
+cout<<"\n\n";
+for(j=0;j<3;j++)
+cout<<y[i][j]<<"\t";
+}
+for(i=0;i<3;i++)
+{
+for(j=0;j<3;j++)
+z[i][j]=x[i][j]+y[i][j];
+}
+cout<<"\nMATRIX [Z]";
+for(i=0;i<3;i++)
+{
+cout<<"\n\n";
+for(j=0;j<3;j++)
+cout<<z[i][j]<<"\t";
+}
+}
+
