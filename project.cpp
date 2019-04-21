86b56417aab9700ad88fc46d0f9f6462bb6b8d83
Ma9si <46700948+Ma9si@users.noreply.github.com>
2019-04-22 00:28:50 +0530

    Delete element in array

--- project.cpp
+++ project.cpp
@@ -361,4 +361,41 @@ int insert()                                                       //function to
   }
 }
 
+/*............................................*/
+
+int Delete()                                                     //function to delete an element in array
+{
+	int i,a[5],no,pos;
+   cout<<"Enter Data in Array: ";
+   for(i=0;i<5;i++)
+   {
+    cin>>a[i];
+   }
+   cout<<"\n\nStored Data in Array:  ";
+   for(i=0;i<5;i++)
+   {
+    cout<<a[i];
+   }
+   cout<<"\n\nEnter poss. of Element to Delete: ";
+   cin>>pos;
+   if(pos>5)
+   {
+   cout<<"\n\nThis value is out of range: ";
+   }
+   else
+   {
+   --pos;
+   for(i=pos;i<=4;i++)
+   {
+    a[i]=a[i+1];
+   }
+   cout<<"\n\nNew Data in Array: ";
+   for(i=0;i<4;i++)
+   {
+    cout<<a[i];
+  }
+  }
+}
+
+
 
