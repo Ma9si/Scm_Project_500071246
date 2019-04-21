c17bbfcea05c14e24e031338bf9d48ba73b17716
Ma9si <46700948+Ma9si@users.noreply.github.com>
2019-04-22 01:15:58 +0530

    Compare strings

--- project.cpp
+++ project.cpp
@@ -448,7 +448,7 @@ int sumelement()                                                   //function to
 
 /*...............................................*/
 
-int length()                                        //function to find length of string
+int length()                                            //function to find length of string
 {
 	int i,count=0;
    char ch[20];
@@ -460,6 +460,50 @@ int length()                                        //function to find length of
    count++;
    }
    cout<<"String Length: "<<count;
+}
+
+/*................................................*/
+
+int compare()                                            //function to compare two strings
+{
+	 char str1[20],str2[20],i,j,flag=0;
+ clrscr();
+ cout<<"Enter first string: ";
+ gets(str1);
+ cout<<"Enter Second string: ";
+ gets(str2);
+ i=0;
+ j=0;
+  while(str1[i]!='\0')
+  {
+   i++;
+  }
+  while(str2[j]!='\0')
+  {
+   j++;
+  }
+ if(i!=j)
+ {
+ flag=0;
+ }
+ else
+ {
+ for(i=0,j=0;str1[i]!='\0',str2[j]!='\0';i++,j++)
+ {
+ if(str1[i]==str2[j])
+ {
+ flag=1;
+ }
+ }
+ }
+ if(flag==0)
+ {
+ cout<<"Strings are not equal";
+ }
+ else
+ {
+ cout<<"Strings are equal";
+ }
 }
 
 
