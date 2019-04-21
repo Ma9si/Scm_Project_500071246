9ae1fec3ee1f1fd2b3d72cb2178369a67dfafb15
Ma9si <46700948+Ma9si@users.noreply.github.com>
2019-04-22 00:37:37 +0530

    Reverse an array

--- project.cpp
+++ project.cpp
@@ -397,5 +397,30 @@ int Delete()                                                     //function to d
   }
 }
 
+/*.................................................*/
+
+int revarray()                                                     //function to reverse an array
+{
+	int a[20],b[20],i,j,n;
+   cout<<"How many elements you want to enter: ";
+   cin>>n;
+   cout<<"Enter any "<<n<<" elements in Array: ";
+   for(i=0; i<n ;i++)
+   {
+   cin>>a[i];
+   }
+   cout<<"Reverse of Array: ";
+
+   for(i=n-1,j=0; i>=0;i--,j++)
+   {
+    b[i]=a[j];
+   }
+   for(i=0; i<n ;i++)
+   {
+   cout<<b[i];
+   }
+}
+ 
+
 
 
