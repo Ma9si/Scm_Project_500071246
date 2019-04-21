f4cd482d7fc792062311c00a2e5db4502cc2b11a
Ma9si <46700948+Ma9si@users.noreply.github.com>
2019-04-22 04:06:06 +0530

    Stack implemented

--- project.cpp
+++ project.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include "stack_header.h"
 using namespace std;
 
 
@@ -604,6 +605,44 @@ int vowcon()                                               //function to count n
     return 0;
 }	
   
+/*....................................................*/
 
+
+void stack()                                                   //function to implement stack which includes user defined header file
+{
+   int ch, val; 
+   cout<<"1) Push in stack"<<endl;
+   cout<<"2) Pop from stack"<<endl;
+   cout<<"3) Display stack"<<endl;
+   cout<<"4) Exit"<<endl;
+   do {
+      cout<<"Enter choice: "<<endl;
+      cin>>ch;
+      switch(ch) {
+         case 1: {   
+            cout<<"Enter value to be pushed:"<<endl;
+            cin>>val;
+             push(val);
+            break;
+         }
+         case 2: {
+            pop();
+            break;
+         }
+         case 3: {
+            display();
+            break;
+         }
+         case 4: {
+            cout<<"Exit"<<endl;
+            break;
+         }
+         default: {
+            cout<<"Invalid Choice"<<endl;
+         }
+      }
+   }while(ch!=4); 
+    
+}
 
 
