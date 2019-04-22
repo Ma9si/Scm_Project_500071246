fa362513737e1d9f08a07da5144c279a0a8919f5
karan1134 <46700802+karan1134@users.noreply.github.com>
2019-04-23 01:38:59 +0530

    fibonacci series

--- project.cpp
+++ project.cpp
@@ -1025,5 +1025,33 @@ int gcd()
 }
 
 
+/*........................................................*/
+
+
+int fibo()
+{
+	int range, first = 0, second = 1, fibonacci=0;
+	cout << "range";
+	cin>>range;
+	cout << "Fibonacci Series upto " << range << " Terms "<< endl;
+	for ( int c = 0 ; c < range ; c++ ) {
+		if ( c <= 1 )
+		             fibonacci = c; else {
+			fibonacci = first + second;
+			first = second;
+			second = fibonacci;
+		}
+		cout << fibonacci <<" ";
+	}
+	return 0;
+}
+
+
+/*......................................................*/
+
+
+
+
+
 
 
